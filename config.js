9df2d205fba73b341bac53c55dd77599414ebacb
root <thomas12345679@gmail.com>
2018-05-04 08:54:25 +0800

    fix bug

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
-var config = {}
+var config       = {},
+    auth         = require("./auth.js"),
+    parseDatepat = require("./lib/parsedatepattern.js").parseDatepat
 
 config.elhost        = "10.10.80.25",
-config.elport        = "9200", //if you have index pattern, you can use YYYY-MM-dd
-config.index         = "pa001-2018.05.03", 
+config.elport        = "9200",
+config.datepat       = "daily", //index date pattern, support: "daily"
+config.index         = "pa001-" + parseDatepat(config.datepat), 
 config.field         = "value", //agg field, if your query is text, and you don't care about agg, you can omit this
 //config.isCounter     = false, 
 config.queryUnit     = "count"
@@ -15,22 +18,8 @@ config.compareMode   = "hit",//available: "dir" "raw"
 config.singleMode    = false,
 config.exitMode      = false,
 config.onlyMet       = false,
-config.sendMail      = false,
+config.sendMail      = true,
 config.mailService   = "gmail"
-config.mailAuth      = {
-  user: "<your gmail username>",
-  pass: "<your gmail pass>"
-}
-
-
-//YYYY.MM.dd
-function dailyPat() {
-  var t      = new Date()
-  var offset = t.getTimezoneOffset() * 60 * 1000
-  t -= -offset
-  var d      = new Date(t)
-  var mydate = d.getFullYear() + '.' + ('0' + (d.getMonth() + 1)).slice(-2) + '.' + ('0' + d.getDate()).slice(-2)
-  return mydate
-}
+config.mailAuth      = auth
 
 module.exports = config
