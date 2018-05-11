c80d4379edf2c73bd9c2708f1f082205330048c1
tmyl123 <thomas12345679@gmail.com>
2018-05-11 12:48:42 +0800

    remove search pattern, let user customized

--- config.example.js
+++ config.example.js
@@ -7,68 +7,46 @@ config.datepat       = "daily"
 config.index         = "myindex-"
 
 
-//BASIC QUERY SETUP
-config.queryString   = "tag:error"
-config.queryRange    = "@timestamp=gt:now-50m&lt:now,bytes=gt:2000"
-
-
-//AGGREGATION QUERY SETUP
-config.withAggs      = true
-config.field         = ""
-config.queryUnit     = ""
-config.rawUnit       = "" 
-config.timeframe     = ""
-config.rawInterval   = 1
-config.datefield     = "@timestamp"
-
-config.aggs = {
-	"val_per_interval" : {
-    "date_histogram" : {
-      "field" : "@timestamp",
-      "interval" : "5m"
-    },
-    "aggs": {
-      "avg_val": {
-        "avg": {
-          "field": "hit"
-        }
-      },
-      "val_speed": {
-        "derivative": {
-          "buckets_path": "avg_val"
-        }
-      },
-			"speed_bucket_filter": {
-        "bucket_selector": {
-          "buckets_path": {
-            "hitSpeed": "val_speed",
-						"av": "avg_val"
+//QUERYSETUP
+config.postContent = {
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "query_string": {
+            "default_field": "message.keyword",
+            "query": "something interesting*"
+          }
         },
-      "script": "params.hitSpeed !== null && params.hitSpeed > 100 && params.av > 2200"
-    }
-  }
+        {
+          "range": {
+            "logdate": {
+              "gt": "now-1h",
+              "lt": "now"
+            }
+          }
+        }
+      ]
     }
   }
 }
 
 //THRESHOLD DEFINATION
-config.compareMode   = "agg"
+config.compareMode   = "hit"
 config.threshold     = "1"
 config.op            = ">"
 
 
 //MAIL CONFIGURATION
-config.sendMail      = true
+config.sendMail      = false
 config.mailService   = "gmail"
 config.mailUser      = "<tux@gmail.com>"
-config.mailPass      = "<tuxbestpassword>"
+config.mailPass      = "<tuxawesomepassword>"
 config.mailTo        = "<foo@gmail.com>"
 config.mailSubject   = "Met the threshold"
-config.mailbody      = "./mailbodies/general_agg.pug"
+config.mailbody      = "./mailbodies/general.pug"
 
 //MISCELLANEOUS
-config.sortField     = "@timestamp"
-config.sortOrder     = "asc"
 config.onlyMet       = false
 
 module.exports = config
--- eq.js
+++ eq.js
@@ -24,18 +24,12 @@ program
   .option('-h, --elhost [value]', 'Elasticsearch host. Default: 127.0.0.1')
   .option('-p, --elport [value]', 'Elasticsearch port. Default: 9200')
   .option('--index [value]', 'Index pattern. Default: collectd-YYYY.MM.dd')
-  .option('-u, --query-unit [value]', 'Unit use for threshold, accept bit / kbit / mbit / byte / count. Default: count')
-  .option('-U, --raw-unit [value]', 'The original field unit, accept bit / kbit / mbit / byte / count. Default: count')
-  .option('-t, --timeframe [value]', 'Group time bucket per n minute. Default: 1')
-  .option('-I, --raw-interval [value]', 'If your database receive data per n second, put n here. Default: 1')
   .option('--op [value]', 'Support > == !== <. Default >')
-  .option('-q, --query-string [value]', 'host.keyword:Moo_Kibana01,type_instance:ens160 Default: none')
-  .option('-f, --field [value]', 'The field you are interested in. Default: value')
-  .option('-r, --query-range [value]', '@timestamp=gt:now-2m&lt:now,value=gt:1000')
+  //.option('-q, --query-string [value]', 'host.keyword:Moo_Kibana01,type_instance:ens160 Default: none')
+  //.option('-r, --query-range [value]', '@timestamp=gt:now-2m&lt:now,value=gt:1000')
   .option('-T, --threshold [value]', 'You dont say')
   .option('-m, --compare-mode [mode]', 'Accept hit / dir. Default: hit')
   .option('-o, --only-met', 'Output only when threshold is met')
-  .option('-A, --met-action', 'the action if we met the condition')
   .option('-c, --config [value]', 'the config file to use')
 
 
@@ -51,16 +45,6 @@ program.on('--help', function(){
 
 program.parse(process.argv);
 
-
-// elastic use GMT+0
-var t      = new Date(),
-    offset = t.getTimezoneOffset() * 60 * 1000
-    t    -= -offset
-
-// prepend 0 to month and date
-var d = new Date(t)
-var mydate = d.getFullYear() + '.' + ('0' + (d.getMonth() + 1)).slice(-2) + '.' + ('0' + d.getDate()).slice(-2) 
-
 // config goes here
 var configpath = path.join(process.cwd(), program.config)
 var config     = require(configpath)
@@ -70,78 +54,20 @@ var elhost      = program.elhost        || config.elhost,
     elport      = program.elport        || config.elport,
     index       = program.index         || config.index, 
     datefield   = program.datefield     || config.datefield,
-    qfield      = program.field         || config.field,
-    iscounter   = program.isCounter     || config.isCounter,
-    queryunit   = program.queryUnit     || config.queryUnit,
-    rawunit     = program.rawUnit       || config.rawUnit,
-    timeframe   = parseTimeframe(config.timeframe),
     rawinterval = program.rawInterval   || config.rawInterval,
-    //threshold   = parseThreshold(config.threshold),
-    //qthreshold  = threshold.thresholdval / threshold.timesecond,
-		threshold   = config.threshold
+		threshold   = config.threshold,
     op          = program.op            || config.op,
     comparemode = program.compareMode   || config.compareMode,
     onlymet     = program.onlyMet       || config.onlyMet,
     sendmail    = program.sendMail      || config.sendMail,
-    querystring = program.queryString   || config.queryString,
-    queryrange  = program.queryRange    || config.queryRange
-
 
 if (config.datepat) {
   index = index + parseDatepat(config.datepat)
   config.fullIndex = index
 }
 
-var querymust = []
-// making query object
-querystring.split(',').forEach(e => {
-  var termobj = {}
-  termobj.term = {}
-  termobj['term'][e.split(':')[0]] = e.split(':')[1]
-  querymust.push(termobj)
-})
 
-// making range object
-if (queryrange) {
-  queryrange.split(',').forEach(e => {
-    var rangeobj = {}
-    rangeobj.range = {}
-    e.split('=').forEach(a => {
-      field = e.split('=')[0]
-      condition = e.split('=')[1]
-      rangeobj.range[field] = {}
-      condition.split("&").forEach(c => {
-        gtlt = c.split(':')[0]
-        val = c.split(':')[1]
-        rangeobj.range[field][gtlt] = val
-      })
-    })
-    querymust.push(rangeobj)
-  })
-}
-
-
-
-//console.log(threshold.thresholdval, threshold.thresholdunit, threshold.timesecond)
-// content post to elastic
-var postcontent = {
-  "sort":[],
-  "query": {
-    "bool": {
-      "must": querymust
-    }
-  },
-}
-
-if (config.withAggs) {
-  postcontent.aggs = config.aggs
-}
-
-if (config.sortField) {
-	var obj = {}
-	obj.order = config.sortOrder
-  postcontent.sort[0] = {[config.sortField]: obj}
-}
+var postcontent = config.postContent
 
 var options = {
   url: "http://" + elhost + ":" + elport + "/" + index + "/_search",
@@ -171,9 +97,9 @@ request.post(options,function(err, response, body){
   }
 
 	
-  if (body.hits.hits.length > 0) {
-    sumobj.lastvalue = body.hits.hits[body.hits.hits.length-1]["_source"][qfield]
-  }
+//  if (body.hits.hits.length > 0) {
+//    sumobj.lastvalue = body.hits.hits[body.hits.hits.length-1]["_source"][qfield]
+//  }
 
 	if (comparemode == "hit") {
 
